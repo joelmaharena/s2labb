c1e69fe33faf9d620f3ba1046f1a35155e60c46b
Joel Maharena <joelmah@kth.se>
2025-04-28 12:26:17 +0200

    fix

--- funkprog/longestString.hs
+++
@@ -1,8 +0,0 @@
-longestString :: [String] -> String
-longestString [] = []
-longestString (x:xs) = foldl (helpFunc) x xs
-
-helpFunc :: String -> String -> String
-helpFunc x y 
-    | length x >= length y = x
-    | otherwise = y
--- funkprog/nolowercase.hs
+++
@@ -1,6 +0,0 @@
-noLowercaseStrings :: [String] -> [String]
-noLowercaseStrings [] = []
-noLowercaseStrings (x:xs)
-        | x /= filter (`notElem` ['a'..'z']) x = noLowercaseStrings xs
-        | otherwise = x : noLowercaseStrings xs
-
